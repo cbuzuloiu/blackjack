2de98992c69f6e2fb7033ce2cacc3ce12376f677
cosmin <cbuzuloiu@gmail.com>
2025-02-26 23:38:40 +0200

    function to suffle the cards added / CLI

--- script.js
+++ script.js
@@ -1,2 +1,70 @@
 "use strict";
-console.log("Hello World!");
+
+const cards = [
+  "1h",
+  "2h",
+  "3h",
+  "4h",
+  "5h",
+  "6h",
+  "7h",
+  "8h",
+  "9h",
+  "10h",
+  "11h",
+  "12h",
+  "13h",
+  "1d",
+  "2d",
+  "3d",
+  "4d",
+  "5d",
+  "6d",
+  "7d",
+  "8d",
+  "9d",
+  "10d",
+  "11d",
+  "12d",
+  "13d",
+  "1s",
+  "2s",
+  "3s",
+  "4s",
+  "5s",
+  "6s",
+  "7s",
+  "8s",
+  "9s",
+  "10s",
+  "11s",
+  "12s",
+  "13s",
+  "1c",
+  "2c",
+  "3c",
+  "4c",
+  "5c",
+  "6c",
+  "7c",
+  "8c",
+  "9c",
+  "10c",
+  "11c",
+  "12c",
+  "13c",
+];
+
+// shuffleArray
+function createRandomArray(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    // Pick a random index from 0 to i
+    let randomNumber = Math.floor(Math.random() * (i + 1));
+    // Swap elements at indices i and and randomNumber
+    [arr[i], arr[randomNumber]] = [arr[randomNumber], arr[i]];
+  }
+
+  return arr;
+}
+
+console.log(createRandomArray(cards));
